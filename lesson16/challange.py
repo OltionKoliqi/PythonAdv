3b87bff191bbf9eb0101625e66f9a2fa13cfa4e8
Student <oltionkoliqi12@gmail.com>
2025-03-22 04:45:08 -0700

    Merge remote-tracking branch 'origin/master'

--- lesson16/challange.py
+++ lesson16/challange.py
@@ -5,7 +5,7 @@ st.title("Personal Data Form")
 name = st.text_input("Enter your Name")
 surname = st.text_input("Enter your Surname")
 age = st.number_input("Enter your Age", min_value=0, step=1)
-height = st.number_input("Enter your Height ", min_value=0, step=1)
+height = st.number_input("Enter your Height  ", min_value=0, step=1)
 
 if st.button("Send"):
     if name and surname and age and height:
