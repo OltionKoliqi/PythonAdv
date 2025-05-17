b67414bd16ce8d3bc384782ff841d75edcc12fed
Student <koliqiolktioj@gmail.com>
2025-05-17 03:47:10 -0700

    Merge remote-tracking branch 'origin/master'

---
+++ module20_perseritje/module20.py
@@ -0,0 +1,52 @@
+data = {
+    "Name": "Oltion",
+    "Age": 14,
+    "address": {
+        "street": "124th street",
+        "city": "Prishtina"
+    },
+    "contact": [
+        {
+            "type": "email"
+        },
+        {
+            "type": "phone"
+        }
+    ]
+}
+print(data["Name"])
+print(data["address"]["street"])
+print(data["contact"][1]["type"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
