055a55c8f9827558fdb6a90ee8dfd3d4f22aad76
Student <oltionkoliqi12@gmail.com>
2025-09-27 12:43:48 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ API_project/database.py
@@ -0,0 +1,13 @@
+import sqlite3
+import os
+from dotenv import load_dotenv
+
+from API_project.main import DATABASE_URL
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_db_connection():
+    connection = sqlite3.connect(DATABASE_URL)
+    connection.row_factory = sqlite3.Row
+    return connection
---
+++ API_project/main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from lesson24.main import cursor
+from routers import recipes, categories
+import os
+from dotenv import load_dotenv
+from database import get_db_connection
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+app = FastAPI()
+
+app.include_router(categories.router)
+app.include_router(recipes.router)
+
+@app.on_event("startup")
+def startup():
+    conn = get_db_connection()
+    cursor = conn.curosr()
+
+    cursor.execute('''
+        CREATE TABLE if NOT EXISTS categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE if NOT EXISTS recipes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE NOT NULL,
+            description TEXT,
+            ingredients TEXT,
+            cuisine TEXT,
+            difficulty TEXT,
+            category_id INTEGER,
+            FOREIGN KEY (category_id) REFERENCES categories (id)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+@app.get('/')
+def read_root():
+    return {"message": "FastAPI with SQLite project"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
